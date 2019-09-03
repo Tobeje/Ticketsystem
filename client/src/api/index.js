22200fdbb783169c809764eff3cada832d5ec140
Tobeje <schmager29@gmail.com>
2019-09-03 12:53:34 +0200

    changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'http://81.169.181.191:3001/api',
 })
 
 export const insertTicket = payload => api.post(`/ticket`, payload)
