9b43bd9e540c54f8a114b36e71fa96edab404e2b
Tobeje <schmager29@gmail.com>
2019-09-03 11:25:02 +0200

    Init FrontEnd

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import logo from '../ticket_img.jpg'
+
+const Wrapper = styled.a.attrs({
+    className: 'navbar-brand',
+})``
+
+class Logo extends Component {
+    render() {
+        return (
+            <Wrapper href="http://81.169.181.191:8000/">
+                <img src={logo} width="50" height="50" alt="http://81.169.181.191:8000/" />
+            </Wrapper>
+        )
+    }
+}
+
+export default Logo
