c543a25106d86071bc5910ac0ca0c0729cbfb770
Tobeje <schmager29@gmail.com>
2019-09-03 11:59:41 +0200

    Changes

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { NavBar } from '../components'
-import { TicketsList, TicketsInsert, TicketsUpdate } from '../pages'
+import { TicketsList, TicketsInsert, TicketsUpdate, TicketMainPage } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
         <Router>
-            
+
             <Switch>
                 <Route path="/" exact component={TicketMainPage} />
                 <Route path="/tickets/list" exact component={TicketsList} />
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 import TicketsList from './TicketsList'
 import TicketsInsert from './TicketsInsert'
 import TicketsUpdate from './TicketsUpdate'
+import TicketMainPage from './TicketMainPage'
 
-export { TicketsList, TicketsInsert, TicketsUpdate }
+export { TicketsList, TicketsInsert, TicketsUpdate, TicketMainPage }
