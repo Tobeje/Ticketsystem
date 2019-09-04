f7777f3ef984594691a2b7b5df9b11545f7f8570
Tobeje <schmager29@gmail.com>
2019-09-04 08:26:26 +0200

    Update index.js

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-//import { NavBar } from '../components'
-import { TicketsList, TicketsInsert, TicketsUpdate, TicketMainPage } from '../pages'
+import { NavBar } from '../components'
+import { TicketsList, TicketsInsert, TicketsUpdate } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
         <Router>
+        </NavBar>
             <Switch>
-                <Route path="/" exact component={TicketMainPage} />
                 <Route path="/tickets/list" exact component={TicketsList} />
                 <Route path="/tickets/create" exact component={TicketsInsert} />
                 <Route path="/tickets/update/:id" exact component={TicketsUpdate} />
