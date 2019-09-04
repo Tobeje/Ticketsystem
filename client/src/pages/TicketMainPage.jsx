df2e9959dafb99c743ea4cf85d31dfdc9f988264
Tobeje <schmager29@gmail.com>
2019-09-04 08:19:03 +0200

    Creating Mainpage

--- client/src/pages/TicketMainPage.jsx
+++ client/src/pages/TicketMainPage.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 import { NavBar } from '../components'
 
+const box = styled.div.attrs({
+    className: 'navbar navbar-expand-lg navbar-dark bg-dark',
+})`
+width: 50%;
+height: 50%;
+display: inline-block;
+-webkit-box-shadow: 10px -3px 81px -10px rgba(0,0,0,0.86);
+-moz-box-shadow: 10px -3px 81px -10px rgba(0,0,0,0.86);
+box-shadow: 10px -3px 81px -10px rgba(0,0,0,0.86);
+`
+
+const boxContainer = styled.div`
+text-align: center;
+`
+
+const Button = styled.button.attrs({
+    className: `btn btn-primary`,
+})`
+    margin: 15px 15px 15px 5px;
+`
+
 class TicketMainPage extends Component {
     render() {
         return (
-            <div>
-              <NavBar />
-                <p>Welcome to the Main Page of the Ticketsystem</p>
-            </div>
+          <boxContainer>
+            <box>
+              <Button onclick="location.href = '/tickets/create';">Ticket Anlegen</Button>
+              <Button onclick="location.href = '/tickets/list';">Tickets</Button>
+            </box>
+          </boxContainer>
         )
     }
 }
