37c414b124672f4b15c04a241f3e9f357a614fb6
Tobeje <schmager29@gmail.com>
2019-09-04 10:20:24 +0200

    Update TicketsList.jsx

--- client/src/pages/TicketsList.jsx
+++ client/src/pages/TicketsList.jsx
@@ -9,6 +9,12 @@ import 'react-table/react-table.css'
 
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
+    background-color:#FFF;
+    width: 80%;
+    margin: 0 auto;
+    -webkit-box-shadow: 10px -3px 81px -10px rgba(0,0,0,0.86);
+    -moz-box-shadow: 10px -3px 81px -10px rgba(0,0,0,0.86);
+    box-shadow: 10px -3px 81px -10px rgba(0,0,0,0.86);
 `
 
 const Update = styled.div`
