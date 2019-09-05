d0d69fdfe8e197191abef86a63250b6d963b3075
Tobeje <schmager29@gmail.com>
2019-09-05 10:20:14 +0200

    Update TicketsInsert.jsx

--- client/src/pages/TicketsInsert.jsx
+++ client/src/pages/TicketsInsert.jsx
@@ -33,7 +33,19 @@ const InputText = styled.input.attrs({
 `
 
 const InputTextBox = styled.input`
-height: 120px;
+display: block;
+width: 100%;
+height: 80px;
+padding: .375rem .75rem;
+font-size: 1rem;
+font-weight: 400;
+line-height: 1.5;
+color: #495057;
+background-color: #fff;
+background-clip: padding-box;
+border: 1px solid #ced4da;
+border-radius: .25rem;
+transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
 `
 
 const Button = styled.button.attrs({
